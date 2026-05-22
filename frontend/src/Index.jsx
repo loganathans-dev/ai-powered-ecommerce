6ba6da1d129cc648a2123f12e171046120de76a6
Loganathan <loganathan.s@dnaskew.com>
2026-05-22 11:21:00 +0530

    Added the frontend url

---
+++ frontend/src/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const Index = () => {
+  // Directly redirect to the home page
+  return <Navigate to="/" replace />;
+};
+
+export default Index;
